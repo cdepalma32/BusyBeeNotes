3ba4802d7f3ac03e54b2e00d1a1b47ff2a78242d
Crystal D <crystaldepalma@yahoo.com>
2024-05-16 11:48:48 -0400

    Pushing to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const api = require('./Develop/routes/index.js');
 const PORT = process.env.PORT || 3001;
 // Create an instance of the Express application
 const app = express();
